96b81792c2b825d7d69e504f4ab39b50684b4a24
Luiz Gustavo <luizgustavopaliaresdiniz@gmail.com>
2025-11-07 22:57:12 -0300

    powerkeeper dart inicio

---
+++ powerkeeper_dart/bin/powerkeeper_dart.dart
@@ -0,0 +1,5 @@
+import 'package:powerkeeper_dart/powerkeeper_dart.dart' as powerkeeper_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${powerkeeper_dart.calculate()}!');
+}
---
+++ powerkeeper_dart/test/powerkeeper_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:powerkeeper_dart/powerkeeper_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
